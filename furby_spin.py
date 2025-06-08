d4aec85ecd8843659fc83f927941cab54e0dfcae
malbu <malbu@users.noreply.github.com>
2025-06-08 12:53:38 -0400

    first motor test

---
+++ furby_spin.py
@@ -0,0 +1,26 @@
+import RPi.GPIO as GPIO
+from time import sleep
+#using L298N driver
+
+IN1, IN2, ENA = 27, 22, 13  
+GPIO.setmode(GPIO.BCM)
+GPIO.setup([IN1, IN2, ENA], GPIO.OUT)
+
+pwm = GPIO.PWM(ENA, 20000)  
+pwm.start(0)                
+
+try:
+    print("Forward")
+    GPIO.output(IN1, GPIO.HIGH)
+    GPIO.output(IN2, GPIO.LOW)
+    pwm.ChangeDutyCycle(80)  # ~80 % speed
+    sleep(2)
+
+    print("Reverse")
+    GPIO.output(IN1, GPIO.LOW)
+    GPIO.output(IN2, GPIO.HIGH)
+    sleep(2)
+
+finally:
+    pwm.ChangeDutyCycle(0)
+    GPIO.cleanup()

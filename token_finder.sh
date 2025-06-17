827fc22bc86e754930c5fd7f50b2575e3d4d7dd0
malbu <malbu@users.noreply.github.com>
2025-06-17 15:26:15 -0400

    added token finder so I can suppress unwanted tokens

---
+++ token_finder.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+MODEL=~/llama.cpp/models/gemma-2-2b-it-Q4_K_S.gguf
+declare -a BLOCKLIST=('*' '```' ' Question:' ' Answer:' $'\n*')
+
+for s in "${BLOCKLIST[@]}"; do
+  TOKENS=$(~/llama.cpp/build/bin/llama-tokenize -m "$MODEL" -- "$s" \
+            | awk '/^[0-9]+:/ {printf "%s ", $2}')
+  echo -e "'$s'  ->  $TOKENS"
+done

a8112684861f720ca3744dab0ca1a9ac7ad1f2c1
malbu <malbu@users.noreply.github.com>
2025-06-19 17:23:41 -0400

    Update furby_tof_tester.py

--- hardware/furby_tof_tester.py
+++ hardware/furby_tof_tester.py
@@ -21,7 +21,7 @@ vl53 = adafruit_vl53l1x.VL53L1X(i2c)
 
 # config
 vl53.distance_mode = 2        # 1 = long (up to 4m), 2 = short (better accuracy at <1.3m)
-vl53.timing_budget = 1000      # milliseconds
+vl53.timing_budget = 50      # milliseconds
 vl53.start_ranging()
 
 
